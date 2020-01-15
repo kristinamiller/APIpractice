ec77f229f1ef759479cff9adb9da51e5e744087e
kristinamiller <kfm287@gmail.com>
2020-01-15 11:50:26 -0800

    added useEffect

--- src/components/TodosIndex.jsx
+++ src/components/TodosIndex.jsx
@@ -1,25 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const TodosIndex = () => {
-  // const [data, setData] = useState({ posts: "" });
+  const [data, setData] = useState(null);
+  const [todos, setTodos] = useState(null)
 
   // const { posts } = data;
 
   const onClickAxios = async e => {
     const res = await axios.get("http://jsonplaceholder.typicode.com/posts/1");
-    console.log(res);
+    // console.log(res);
     // setData({ ...data, posts: res });
   };
 
   const onClickFetch = (e) => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then(response => {
-        const fetchResponse = response.json();
-        return fetchResponse;
-      }) //turns response into json object?
-      .then(json => console.log(json)); //prints it
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
+          .then(response => response.json())
+          .then(json => setTodos(json)); 
   }
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch("http://jsonplaceholder.typicode.com/posts/2");
+  //     res
+  //       .json()
+  //       .then(res => setData(res))
+  //   }
+  //   fetchData();
+  // })
+
+    useEffect(() => {
+      axios
+        .get("http://jsonplaceholder.typicode.com/posts/2")
+        .then(response => console.log(response))
+    })
 
   //rewrite this with a promise ^^
 
@@ -27,8 +41,11 @@ const TodosIndex = () => {
     <div>
       <h1>Todos Index</h1>
       <button onClick={e => onClickAxios(e)}>Click Me Axios</button>
-      <br/>
+      <br />
       <button onClick={e => onClickFetch(e)}>Click Me Fetch</button>
+      <div>Todo Title: {!todos ? null : todos.title}</div>
+      <div>Data: {!data ? null : data.title}</div>
+
       {/* {fetchResponse.title} */}
       {/* {posts.title} */}
     </div>
